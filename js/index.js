ec68b7b000e1ee9744ddc8fb419dd5ad632c6ed9
Samuel Reams <34895348+SamuelReams@users.noreply.github.com>
2018-11-11 20:19:51 -0600

    Added function so output bar doesn't disappear
    it's still sloppy but i dont give two shits

--- js/index.js
+++ js/index.js
@@ -1,57 +1,83 @@
+let begin = true;
+function nope() {
+	if(begin==true) {
+		document.getElementById('i').innerHTML = '';
+		begin = false;
+	}
+}
+
 function one() {
+	nope();
 	document.getElementById('i').innerHTML =  document.getElementById('i').innerHTML + '1';
 }
 function two() {
+	nope();
 	document.getElementById('i').innerHTML = document.getElementById('i').innerHTML + '2';
 }
 function three() {
+	nope();
 	document.getElementById('i').innerHTML = document.getElementById('i').innerHTML + '3';
 }
 function four() {
+	nope();
 	document.getElementById('i').innerHTML = document.getElementById('i').innerHTML + '4';
 }
 function five() {
+	nope();
 	document.getElementById('i').innerHTML = document.getElementById('i').innerHTML + '5';
 }
 function six() {
+	nope();
 	document.getElementById('i').innerHTML = document.getElementById('i').innerHTML + '6';
 }
 function seven() {
+	nope();
 	document.getElementById('i').innerHTML = document.getElementById('i').innerHTML + '7';
 }
 function eight() {
+	nope();
 	document.getElementById('i').innerHTML = document.getElementById('i').innerHTML + '8';
 }
 function nine() {
+	nope();
 	document.getElementById('i').innerHTML = document.getElementById('i').innerHTML + '9';
 }
 function zero() {
+	nope();
 	document.getElementById('i').innerHTML = document.getElementById('i').innerHTML + '0';
 }
 function plus() {
+	nope();
 	document.getElementById('i').innerHTML = document.getElementById('i').innerHTML + '+';
 }
 function minus() {
+	nope();
 	document.getElementById('i').innerHTML = document.getElementById('i').innerHTML + '-';
 }
 function multiplication() {
+	nope();
 	document.getElementById('i').innerHTML = document.getElementById('i').innerHTML + '*';
 }
 function division() {
+	nope();
 	document.getElementById('i').innerHTML = document.getElementById('i').innerHTML + '/';
 }
 function clearThing() {
+	nope();
 	var thingy = document.getElementById('i');
-	thingy.innerHTML = ' ';
+	thingy.innerHTML = '0';
+	begin = true;
 }
 function table() {
+	nope();
 	document.getElementById('i').innerHTML = document.getElementById('i').innerHTML + '.';
 }
 function show() {
-
+	nope();
 	var newValue = eval(document.getElementById('i').innerHTML);
 	document.getElementById('i').innerHTML = newValue;
 }
 function equal() {
+	nope();
 	show();
 }
